544b425594333ca95951d1f53920d582e121f6bb
Olle Ronstad <olle.ronstad@gmail.com>
2024-01-25 10:38:04 +0100

    added read counter function

--- lab_1_realtime_systems/main.c
+++ lab_1_realtime_systems/main.c
@@ -12,22 +12,37 @@
 #include "primes.h"
 
 
-void write_to_counter(uint16_t val){
-	// writing the high byte to tmp
-	TCNT1H = (uint8_t)(val>>8);
-	// writing the low bytes to lower, causeing temp to become higher;
-	TCNT1L = (uint8_t)val;
+static uint16_t counter_plus_0_5_sec;
+
+void setupTIMER(){
+		// use the 8 MHz system clock
+		// prescaler 256
+		// clkio/256 = 0b100 = 0x4
+		TCCR1B = 0x4 << CS10;
 }
 
-void write_to_timer_compare(uint16_t val){
-	OCR1A = val;
+uint16_t read_counter(){
+	// reading lower bit, saves higer bit to tmp
+	uint8_t lower = TCNT1L;
+	// reading higer bit from tmp
+	uint8_t higer = TCNT1H;
+	return ((uint16_t)higer<<8) | (uint16_t)lower;
 }
+
+uint8_t passed_0_5_sec(){
+	
+}
+
 int main(void)
 {	
+	setupTIMER();
 	setupLCD();
+	
 	while (1)
 	{
-		toggle_s1();
+		if (passed_0_5_sec()){
+			toggle_s1();
+		}
 		writeLong(prime);
 		next_prime();
 		for (uint8_t i = 0; i<40; i++)
